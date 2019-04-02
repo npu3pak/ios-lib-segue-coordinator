53f7575587ba18c4834e91196a997cb1a1dd6ce8
Safronov Evgeniy <evsafronov.personal@yandex.ru>
2019-04-02 09:48:25 +0300

    Missed documentation added

--- SegueCoordinator/Classes/SegueCoordinator.swift
+++ SegueCoordinator/Classes/SegueCoordinator.swift
@@ -1,11 +1,22 @@
 
 import UIKit
 
+/**
+ Builds on top of the storyboard and encapsulates navigation logic. View controllers delegates navigation requests to it and SegueCoordinator performs all required transitions as well as data transfer. Typically you should use its subclasses instead.
+*/
+
 open class SegueCoordinator {
 
-    // MARK: Properties
+    // MARK: Public properties
 
+    /**
+     Reference to the storyboard the SegueCoordinator work with.
+    */
     public let storyboard: UIStoryboard
+
+    /**
+     Root navigation controller that was passed into initializer of SegueCoordinator.
+     */
     public let rootNavigationController: UINavigationController
 
     var seguePreparationActions = [String: (UIViewController) -> Void]()
